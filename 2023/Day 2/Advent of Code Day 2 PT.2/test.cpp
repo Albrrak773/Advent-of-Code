30db4965f2ec105b12fe307de156c976c842d4bf
Albrrak773 <65139019+Albrrak773@users.noreply.github.com>
2024-11-07 14:07:26 +0300

    Delete 2023/Day 2/Advent of Code Day 2 PT.2 directory

--- 2023/Day 2/Advent of Code Day 2 PT.2/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main(){
-    string s = "Game 100: 1 blue, 13 green, 14 red; 11 green, 11 blue, 7 red; 2 red, 1 blue, 2 green; 10 blue, 15 red";
-    cout << stoi(s.substr(5,s.find(":") - 5)) << ".";
-    return 0;
-}
